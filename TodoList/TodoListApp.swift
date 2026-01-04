d8f4d9d06ab34e2e00e3c304dc6e345775cfd913
Bakhytgali <omgwtfwat@gmail.com>
2026-01-04 18:18:41 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Rakhat Bakhytgali on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
